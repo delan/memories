88ed0e2f93a1a5b38d34a1f4d9d973440c65f52b
the6p4c <me@doggirl.gay>
2024-02-11 14:29:37 +1000

    format on save, format all files

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
 export const FILTER = /[.](jpe?g|png|webp|mov|mp4)$/i;
 export const VIDEO = /[.](mov|mp4)$/i;
-export const BIG = (path: string) => `https://bucket.daz.cat/08a3d825b3ca1e53/${path}`;
-export const SMALL = (path: string) => `https://bucket.daz.cat/08a3d825b3ca1e53/${path}.jpg`;
+export const BIG = (path: string) =>
+  `https://bucket.daz.cat/08a3d825b3ca1e53/${path}`;
+export const SMALL = (path: string) =>
+  `https://bucket.daz.cat/08a3d825b3ca1e53/${path}.jpg`;
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './main.sass'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./main.sass";
 
-import { Memories } from './Memories.tsx'
+import { Memories } from "./Memories.tsx";
 import { fetchItemMetas } from "./data";
 import { FILTER } from "./config";
 
@@ -10,11 +10,11 @@ async function main() {
   const all = await fetchItemMetas();
   const items = all.filter(({ path }) => FILTER.test(path));
 
-  ReactDOM.createRoot(document.getElementById('root')!).render(
+  ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
       <Memories items={items} />
     </React.StrictMode>,
-  )
+  );
 }
 
-main()
+main();
